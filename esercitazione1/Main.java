988f36171965afe4446993833148c28cfb15e582
not_nimo <damiano.livoni@gmail.com>
2026-01-26 11:58:10 +0000

    esercitazione  1 done

---
+++ esercitazione1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    Persona person1 = new Persona(25, "John", "Doe", 180);
+    Persona person2 = new Persona(30, "Jane", "Smith", 165);
+    Persona person3 = new Persona(22, "Alice", "Johnson", 170);
+    Persona person4 = new Persona(28, "Bob", "Brown", 175);
+
+    person1.printInfo();
+    person2.printInfo();
+    person3.printInfo();
+    person4.printInfo();
+  }
+}
---
+++ esercitazione1/Persona.java
@@ -0,0 +1,20 @@
+public class Persona {
+  public int age;
+  public String name;
+  public String surname;
+  public int height;
+
+  public Persona(int age, String name, String surname, int height) {
+    this.age = age;
+    this.name = name;
+    this.surname = surname;
+    this.height = height;
+  }
+
+  public void printInfo() {
+    System.out.println("Name: " + name + " " + surname);
+    System.out.println("Age: " + age);
+    System.out.println("Height: " + height + " cm");
+    System.out.println("---------------");
+  }
+}
